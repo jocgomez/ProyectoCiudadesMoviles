afaacb05cadb6f9eb2e6a529c404b7c26d71bd2b
Mateo8585 <42383450+Mateo8585@users.noreply.github.com>
2020-11-30 19:34:35 -0500

    Cambie el campo de subir imagen y valide los demás campos como obligatorios

--- ciudadesmoviles/lib/Paginas/Establecimiento.dart
+++ ciudadesmoviles/lib/Paginas/Establecimiento.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:ciudadesmoviles/Componentes/boton.dart';
 import 'package:ciudadesmoviles/Estilos/Estilos.dart';
 import 'package:ciudadesmoviles/Modelos/Tienda.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 
 class Establecimientos extends StatefulWidget {
   _EstablecimientoState createState() => _EstablecimientoState();
@@ -19,6 +24,8 @@ class _EstablecimientoState extends State<Establecimientos> {
   int longitudE;
   int latitudE;
   int ocupadoE = 0;
+
+  File _imagenSubida;
 
   @override
   void initState() {
@@ -85,13 +92,18 @@ class _EstablecimientoState extends State<Establecimientos> {
                   alignment: Alignment.center,
                   width: 360,
                   height: 50,
-                  child: TextField(
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(filled: true, hintText: "Foto"),
-                    onChanged: (valor) {
-                      fotoE = valor;
-                    },
-                  ),
+                  child: BotonAtomo(
+                    
+                    color: Estilos.colorazul, 
+                    estiloTexto: Estilos.estiloTextoBoton,
+                    texto: "Subir imagen tienda                                                        ", 
+                    colorBorde: Estilos.bordeBoton, 
+                    funcion: (){
+
+                       _subirImagen();
+
+                    }),
+              
                 ),
                 SizedBox(height: 20),
                 Container(
@@ -172,6 +184,43 @@ class _EstablecimientoState extends State<Establecimientos> {
   } //Fin Widget
 
   void _guardarDatos() async {
+    
+    if(nombreE == null){
+
+      _mensajeAdvertencia("Ingresar el nombre de la tienda");
+      print("Entro nombre");
+
+    }else if(direccionE == null){
+
+      _mensajeAdvertencia("Ingresar la dirección");
+
+    }else if(nitE == null){
+
+      _mensajeAdvertencia("Ingresar el NIT de la tienda");
+      print("Entro nit");
+
+    }else if(capacidadE == null){
+
+      _mensajeAdvertencia("Ingresar la capacidad máxima");
+
+    }else if(calificacionE == null){
+
+      _mensajeAdvertencia("Ingresar la calificación para la tienda");
+
+    }else if(longitudE == null){
+
+      _mensajeAdvertencia("Ingresar el valor de la longitud");
+
+    }else if(latitudE == null){
+
+      _mensajeAdvertencia("Ingresar el valor de la latitud");
+
+    }else if(_imagenSubida == null){
+
+      _mensajeAdvertencia("Subir la foto para la tienda");
+
+    }else{
+
     var json = {};
 
     json['nombre'] = nombreE;
@@ -204,6 +253,92 @@ class _EstablecimientoState extends State<Establecimientos> {
                 ],
               ));
     } //Fin condición if
+  
+  }//Fin condición IF
+  
   } //Fin método
+
+  void _subirImagen() async{
+
+    var imagen = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _imagenSubida = imagen;
+    });
+
+    if(_imagenSubida != null){
+
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text("Confirmación imagen"),
+                content: Text(
+                    "La imagen selecciona fue subida correctamente."),
+                actions: <Widget>[
+                  FlatButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Aceptar"))
+                ],
+              ));
+
+      var nombreImagen = Uuid().v1();
+      var imagenRuta = "/tiendas/$nombreImagen.jpg";
+
+      final StorageReference storageReference = FirebaseStorage().ref().child(imagenRuta);
+
+      final StorageUploadTask uploadTask = storageReference.putFile(_imagenSubida);
+
+      final StreamSubscription<StorageTaskEvent> streamSubscription = uploadTask.events.listen((event) {
+        
+        print('EVENT ${event.type}');
+
+      });
+
+      await uploadTask.onComplete;
+      streamSubscription.cancel();
+
+      //Se guardar el link en el campo foto para la tienda
+      fotoE = (await storageReference.getDownloadURL()).toString();
+
+    }else{
+
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text("Confirmación imagen"),
+                content: Text(
+                    "La imagen selecciona no fue subida correctamente. Vuelva a subir la imagen."),
+                actions: <Widget>[
+                  FlatButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Aceptar"))
+                ],
+              ));
+
+    }//Fin condición if
+
+  }//Fin método
+
+  void _mensajeAdvertencia(String cadena){
+
+    showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text("Advertencia información"),
+                content: Text(cadena),
+                actions: <Widget>[
+                  FlatButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Aceptar"))
+                ],
+              ));
+
+  }//Fin método
 
 }
